4549835665ad6f028abe7ea9af0fd545a4df770f
reppmg <reppmg@gmail.com>
2017-07-04 17:03:44 +0300

    Refactoring

--- src/main/webapp/data.sql
+++ src/main/webapp/data.sql
@@ -1,2 +1,7 @@
-INSERT INTO public.masters (master_id, master_name, phone, mail) VALUES (1, 'Parker Wilson', '+12345678912', 'p.wilson@mail.com');
-INSERT INTO public.pets (pet_id, birthdate, pet_type, pet_name, master) VALUES (1, '1923-11-10', 'dog', 'Hachiko', 1);
+INSERT INTO public.masters (master_name, phone, mail) VALUES ( 'Parker Wilson', '+12345678912', 'p.wilson@mail.com');
+INSERT INTO public.masters (master_name, phone, mail) VALUES ( 'Richard Mozes', '+636468746887', 'r.mozes@mail.com');
+INSERT INTO public.masters (master_name, phone, mail) VALUES ( 'Mark Hoffman', '+635846484858', 'm.hoffman@mail.com');
+INSERT INTO public.masters (master_name, phone, mail) VALUES ( 'Jack Hetherington', '+15173559277', 'hetherington@pa.msu.edu');
+INSERT INTO public.pets (birthdate, pet_type, pet_name, master) VALUES ( '1923-11-10', 'dog', 'Hachiko', 1);
+INSERT INTO public.pets (birthdate, pet_type, pet_name, master) VALUES ( '1970-8-10', 'dog', 'Rex', 2);
+INSERT INTO public.pets (birthdate, pet_type, pet_name, master) VALUES ( '1972-1-11', 'cat', 'F.D.C. Willard', 4);
--- src/main/webapp/schema.sql
+++ src/main/webapp/schema.sql
@@ -2,8 +2,6 @@ CREATE DATABASE "Pets"
 WITH
 OWNER = postgres
 ENCODING = 'UTF8'
-LC_COLLATE = 'Russian_Russia.1251'
-LC_CTYPE = 'Russian_Russia.1251'
 TABLESPACE = pg_default
 CONNECTION LIMIT = -1;
 
